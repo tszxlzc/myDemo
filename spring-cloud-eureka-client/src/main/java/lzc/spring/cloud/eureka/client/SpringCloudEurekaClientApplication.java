c495c0e4c2a84f0ba2ac23356379d8c3a9c85094
刘宗昌 <liuzongchang@kaike.la>
2018-10-26 17:09:16 +0800

    spring cloud eureka demo

---
+++ spring-cloud-eureka-client/src/main/java/lzc/spring/cloud/eureka/client/SpringCloudEurekaClientApplication.java
@@ -0,0 +1,27 @@
+package lzc.spring.cloud.eureka.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringCloudEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@GetMapping("hi")
+	public String home(@RequestParam(value = "name", defaultValue = "lzc")String name){
+		return "hi" + name + ", i am from port:" + port;
+	}
+}
