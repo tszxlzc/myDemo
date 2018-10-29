257979eea68df2f57aeb4366b24c179b259d8526
刘宗昌 <liuzongchang@kaike.la>
2018-10-29 16:53:10 +0800

    添加spring cloud bus

--- spring-cloud-config-client/src/main/java/lzc/spring/cloud/config/client/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/lzc/spring/cloud/config/client/SpringCloudConfigClientApplication.java
@@ -3,11 +3,17 @@ package lzc.spring.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
