e03afd90da24889099ae36397c5352b6c03152d9
liuzc <zongchang.liu@zhiyoubao.com>
2020-03-23 22:49:20 +0800

    原地移除

--- general-demo/src/main/java/lzc/demo/algorithm/leetcode/RemoveDuplicates.java
+++ general-demo/src/main/java/lzc/demo/algorithm/leetcode/RemoveDuplicates.java
@@ -14,6 +14,29 @@ public class RemoveDuplicates {
 
     }
 
+    /**
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     *
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     *
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     * @param nums
+     * @return
+     */
+    private static int remove(int[] nums, int val){
+        int p= 0;
+        int q= 0;
+        while (q< nums.length){
+            if(nums[q] == val){
+                q++;
+            }else {
+                nums[p++] = nums[q++];
+            }
+        }
+
+        return p;
+    }
+
     private static int remove(int[] nums){
         int p=0;
         int q= 1;
