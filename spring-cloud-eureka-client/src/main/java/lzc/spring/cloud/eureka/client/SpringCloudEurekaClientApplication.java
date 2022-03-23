175747eef823aef033a9c91964596aa5925349af
tszxlzc <tszxlzc@users.noreply.github.com>
2022-03-23 10:47:11 +0800

    eureka客户端不用@EnableEurekaClient这个注解也可以生效

--- spring-cloud-eureka-client/src/main/java/lzc/spring/cloud/eureka/client/SpringCloudEurekaClientApplication.java
+++ spring-cloud-eureka-client/src/main/java/lzc/spring/cloud/eureka/client/SpringCloudEurekaClientApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @RestController
 public class SpringCloudEurekaClientApplication {
 
